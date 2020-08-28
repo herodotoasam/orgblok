d1ef7b4fe0295df5fa9e42ed420215ea04a7c64b
hero <hero@intel-debian.www.rx.com>
2020-08-28 20:14:36 -0300

    hoy

--- init.el
+++ init.el
@@ -249,13 +249,16 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-enabled-themes (quote (leuven)))
+ '(custom-enabled-themes (quote (material)))
  '(custom-safe-themes
    (quote
     ("d4f8fcc20d4b44bf5796196dbeabec42078c2ddb16dcb6ec145a1c610e0842f3" default)))
  '(fci-rule-color "#37474f")
  '(hl-sexp-background-color "#1c1f26")
  '(org-agenda-files (quote ("~/orgblok/*.org")))
+ '(package-selected-packages
+   (quote
+    (material-theme ag auto-complete autopair flx-ido ido-vertical-mode org-bullets powerline better-defaults)))
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
    (quote
