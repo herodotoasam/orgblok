5e5a335f6fae1df09d81cf768a3ca5b669c1d972
heroasam <hfj024@gmail.com>
2021-07-19 16:58:02 -0300

    hoy

--- init.el
+++ init.el
@@ -1,247 +1,116 @@
-;; .emacs.d/init.el
+;; You will most likely need to adjust this font size for your system!
+(defvar efs/default-font-size 180)
+(defvar efs/default-variable-font-size 180)
 
+;; Make frame transparency overridable
+(defvar efs/frame-transparency '(90 . 90))
 
-;; ===================================
-
-;; MELPA Package Support
-
-;; ===================================
-
-;; Enables basic packaging support
-
-(setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-                         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
-
-(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")))
+;; Initialize package sources
 (require 'package)
 
-
-;; Adds the Melpa archive to the list of available repositories
-
-(add-to-list 'package-archives
-                '(("melpa" . "http://melpa.org/packages/")
-                  ("melpa-stable" . "http://stable.melpa.org/packages/")
-                  ("org-elpa" . "https://orgmode.org/elpa/")))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
-(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
-
-
-;; If there are no archived package contents, refresh them
-
-(when (not package-archive-contents)
-
+(unless package-archive-contents
   (package-refresh-contents))
 
-
-;; Installs packages
-
-;;
-
-;; myPackages contains a list of package names
-
-(defvar myPackages
-
-  '(better-defaults                 ;; Set up some better Emacs defaults
-    ;;elpy                            ;; Emacs Lisp Python Environment
-    ;;flycheck                        ;; On the fly syntax checking
-    ;;py-autopep8                     ;; Run autopep8 on save
-    ;;blacken                         ;; Black formatting on save
-    ;;magit                           ;; Git integration
-    ;;enh-ruby-mode
-    
-    ;;web-mode
-    powerline
-
-     org
-    org-bullets
-    ido-vertical-mode
-    flx-ido
-    ;;autopair
-    auto-complete
-    ag
-    material-theme                  ;; Theme
-    evil
-    emmet-mode
-    
-    )
-
-  )
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
 
 
-;; Scans the list in myPackages
+(setq inhibit-startup-message t)
 
-;; If the package listed is not already installed, install it
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+(tooltip-mode -1)           ; Disable tooltips
+(set-fringe-mode 10)        ; Give some breathing room
 
-(mapc #'(lambda (package)
+;;(menu-bar-mode -1)            ; Disable the menu bar
 
-          (unless (package-installed-p package)
+;; Set up the visible bell
+(setq visible-bell t)
 
-            (package-install package)))
+(column-number-mode)
+(global-display-line-numbers-mode t)
 
-      myPackages)
+;; Set frame transparency
+(set-frame-parameter (selected-frame) 'alpha efs/frame-transparency)
+(add-to-list 'default-frame-alist `(alpha . ,efs/frame-transparency))
+(set-frame-parameter (selected-frame) 'fullscreen 'maximized)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
 
-;; ===================================
-
-;; Basic Customization
-
-;; ===================================
-
-
-(setq inhibit-startup-message t)    ;; Hide the startup message
-
-(load-theme 'material t)            ;; Load material theme
-
-;;(global-linum-mode t)               ;; Enable line numbers globally
-
-;;(elpy-enable)
-
-;; Enable Flycheck
-
-;;(when (require 'flycheck nil t)
-
-;;  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-
-;;  (add-hook 'elpy-mode-hook 'flycheck-mode))
-
-;; Enable autopep8
-
-;;(require 'py-autopep8)
-
-;;(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+                treemacs-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 
+(set-face-attribute 'default nil :font "Fira Code Retina" :height efs/default-font-size)
 
-;; User-Defined init.el ends here
+;; Set the fixed pitch face
+(set-face-attribute 'fixed-pitch nil :font "Fira Code Retina" :height efs/default-font-size)
 
-(global-set-key (kbd "C-<right>") 'next-buffer)
-(global-set-key (kbd "C-<left>") 'previous-buffer)
-(global-set-key (kbd "C-<up>") 'delete-other-windows)
-
-
-(defun org-insert-inactive-timestamp()
-  "Inserta un timestamp inactive"
-  (interactive)
-  (org-insert-time-stamp nil nil t)
-  )
-(global-set-key (kbd "C-{") 'org-insert-inactive-timestamp)
-(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-(global-set-key (kbd "C-}") 'org-agenda)
-(add-hook 'org-mode-hook 'turn-on-auto-fill)
-(defun nolinum ()
-  (global-linum-mode 0)
-)
-(add-hook 'org-mode-hook 'nolinum)
-
-;;(add-hook 'tcl-mode-hook (lambda () (auto-complete-mode 1)))
-;;(add-hook 'tcl-mode-hook (lambda () (linum-mode 1)))
-;;(add-hook 'tcl-mode-hook (lambda () (hl-line-mode 1)))
-;;(add-hook 'tcl-mode-hook (lambda () (show-paren-mode 1)))
-;;(add-hook 'tcl-mode-hook 'autopair-mode)
-
-;; (require 'js2-mode)
-;; (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-;; ;; Better imenu
-;; (add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
-
-;; (add-hook 'js2-mode-hook (lambda () (auto-complete-mode 1)))
-;; (add-hook 'js2-mode-hook (lambda () (linum-mode 1)))
-;; (add-hook 'js2-mode-hook (lambda () (hl-line-mode 1)))
-;; (add-hook 'js2-mode-hook (lambda () (show-paren-mode 1)))
-;; (add-hook 'js2-mode-hook 'autopair-mode)
-
-(add-hook 'python-mode-hook (lambda () (linum-mode 1)))
-(add-hook 'python-mode-hook (lambda () (auto-complete-mode 1)))
-(add-hook 'python-mode-hook (lambda () (hl-line-mode 1)))
-(add-hook 'python-mode-hook (lambda () (show-paren-mode 1)))
-(add-hook 'python-mode-hook 'autopair-mode)
-
-(fset 'yes-or-no-p 'y-or-n-p)
-
-;;(require 'web-mode)
-;;(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-
-;;(defun my-web-mode-hook ()
-;;  "Hooks for Web mode."
-;;  (setq web-mode-markup-indent-offset 2)
-;;)
-;;(add-hook 'web-mode-hook  'my-web-mode-hook)
-;;(add-hook 'web-mode-hook (lambda () (linum-mode 1)))
-;;(add-hook 'web-mode-hook (lambda () (auto-complete-mode 1)))
-;;(add-hook 'web-mode-hook (lambda () (hl-line-mode 1)))
-;;(add-hook 'web-mode-hook (lambda () (show-paren-mode 1)))
-;;(add-hook 'web-mode-hook (lambda () (emmet-mode 1)))
-;;(add-hook 'web-mode-hook 'autopair-mode)
+;; Set the variable pitch face
+(set-face-attribute 'variable-pitch nil :font "Cantarell" :height efs/default-variable-font-size :weight 'regular)
 
 
-;; Duplicate line
-(defun duplicate-line (arg)
-  "Duplicate current line, leaving point in lower line."
-  (interactive "*p")
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-  ;; save the point for undo
-  (setq buffer-undo-list (cons (point) buffer-undo-list))
+(use-package general
+  :config
+  (general-create-definer rune/leader-keys
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC")
 
-  ;; local variables for start and end of line
-  (let ((bol (save-excursion (beginning-of-line) (point)))
-        eol)
-    (save-excursion
+  (rune/leader-keys
+    "t"  '(:ignore t :which-key "toggles")
+    "tt" '(counsel-load-theme :which-key "choose theme")))
 
-      ;; don't use forward-line for this, because you would have
-      ;; to check whether you are at the end of the buffer
-      (end-of-line)
-      (setq eol (point))
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
 
-      ;; store the line and disable the recording of undo information
-      (let ((line (buffer-substring bol eol))
-            (buffer-undo-list t)
-            (count arg))
-        ;; insert the line arg times
-        (while (> count 0)
-          (newline)         ;; because there is no newline in 'line'
-          (insert line)
-          (setq count (1- count)))
-        )
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
 
-      ;; create the undo information
-      (setq buffer-undo-list (cons (cons eol (point)) buffer-undo-list)))
-    ) ; end-of-let
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
 
-  ;; put the point in the lowest line and return
-  (next-line arg))
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
 
-(global-set-key (kbd "C-S-d") 'duplicate-line)
+(use-package doom-themes
+  :init (load-theme 'doom-dracula t))
 
-(defun move-line (n)
-  "Move the current line up or down by N lines."
-  (interactive "p")
-  (setq col (current-column))
-  (beginning-of-line) (setq start (point))
-  (end-of-line) (forward-char) (setq end (point))
-  (let ((line-text (delete-and-extract-region start end)))
-    (forward-line n)
-    (insert line-text)
-    ;; restore point to original column in moved line
-    (forward-line -1)
-    (forward-char col)))
 
-(defun move-line-up (n)
-  "Move the current line up by N lines."
-  (interactive "p")
-  (move-line (if (null n) -1 (- n))))
+(use-package all-the-icons)
 
-(defun move-line-down (n)
-  "Move the current line down by N lines."
-  (interactive "p")
-  (move-line (if (null n) 1 n)))
 
-(global-set-key (kbd "M-<up>") 'move-line-up)
-(global-set-key (kbd "M-<down>") 'move-line-down)
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 1))
 
-(desktop-save-mode 1)
-(recentf-mode 1)
-(setq recentf-max-menu-items 25)
 ;; ido mo
 (require 'flx-ido)
 (setq ido-enable-flex-maching t)
@@ -251,70 +120,96 @@
 (setq ido-ignore-buffers '("\\` " "^\*" "\*.\*~"))
 (ido-mode 1)
 
-(require 'ag)
-;;(require 'evil)
-;;(evil-mode 1)
 
+
+(defun efs/org-font-setup ()
+  ;; Replace list hyphen with dot
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+  ;; Set faces for heading levels
+  (dolist (face '((org-level-1 . 1.2)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
+
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
+
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+
+(defun efs/org-mode-visual-fill ()
+  (setq visual-fill-column-width 100
+        visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . efs/org-mode-visual-fill))
+
+
+;; Mis propios ajustes 
+(global-set-key (kbd "C-<right>") 'next-buffer)
+(global-set-key (kbd "C-<left>") 'previous-buffer)
+(global-set-key (kbd "C-<up>") 'delete-other-windows)
+
+
+;; Mis propios ajustes de orgmode 
+(defun org-insert-inactive-timestamp()
+  "Inserta un timestamp inactive"
+  (interactive)
+  (org-insert-time-stamp nil nil t)
+  )
+(global-set-key (kbd "C-{") 'org-insert-inactive-timestamp)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+(global-set-key (kbd "C-}") 'org-agenda)
+(add-hook 'org-mode-hook 'turn-on-auto-fill)
 
 
 (require 'powerline)
 (powerline-default-theme)
-;;(require 'powerline-evil)
-;; Enable Evil
-(require 'evil)
-(evil-mode 1)
-;; Enable emmet
-(require 'emmet-mode)
+;;(powerline-center-evil-theme)
 
+;;;python-elpy
+(require 'elpy)
+(elpy-enable)
+(setq elpy-rpc-python-command "python3")
 
-;; --------------------------------------------
-;; ACA termina lo que yo he programado
-;; --------------------------------------------
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Input Mono" :foundry "FBI " :slant normal :weight normal :height 143 :width normal)))))
+(require 'ag)
+(projectile-mode +1)
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
+;;; END ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-enabled-themes (quote (material)))
- '(custom-safe-themes
-   (quote
-    ("d4f8fcc20d4b44bf5796196dbeabec42078c2ddb16dcb6ec145a1c610e0842f3" default)))
- '(fci-rule-color "#37474f")
- '(hl-sexp-background-color "#1c1f26")
- '(org-agenda-files (quote ("~/orgblok/*.org")))
  '(package-selected-packages
    (quote
-    (emmet-mode evil material-theme ag auto-complete autopair flx-ido ido-vertical-mode org-bullets powerline better-defaults)))
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#f36c60")
-     (40 . "#ff9800")
-     (60 . "#fff59d")
-     (80 . "#8bc34a")
-     (100 . "#81d4fa")
-     (120 . "#4dd0e1")
-     (140 . "#b39ddb")
-     (160 . "#f36c60")
-     (180 . "#ff9800")
-     (200 . "#fff59d")
-     (220 . "#8bc34a")
-     (240 . "#81d4fa")
-     (260 . "#4dd0e1")
-     (280 . "#b39ddb")
-     (300 . "#f36c60")
-     (320 . "#ff9800")
-     (340 . "#fff59d")
-     (360 . "#8bc34a"))))
- '(vc-annotate-very-old-color nil))
+    (vterm which-key visual-fill-column use-package python-mode py-autopep8 powerline org-bullets material-theme magit ivy-rich ido-vertical-mode general flycheck flx-ido evil-collection emmet-mode elpy doom-themes doom-modeline counsel company-box blacken better-defaults auto-complete ag))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
