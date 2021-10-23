9514606168a1fc8c8dd6b28f0bec5a9ca57730e3
heroasam <hfj024@gmail.com>
2021-10-23 17:24:02 -0300

    hoy

--- init.el
+++ init.el
@@ -313,20 +313,20 @@
 (setq gc-cons-threshold (* 2 1000 1000))
 
 ;; org-roam
-(use-package org-roam
-  :ensure t
-  :init
-  (setq org-roam-v2-ack t)
-  :custom
-  (org-roam-directory "~/orgblok/roam")
-  (org-roam-completion-everywhere t)
-  :bind (("C-c n l" . org-roam-buffer-toggle)
-	 ("C-c n f" . org-roam-node-find)
-	 ("C-c n i" . org-roam-node-insert)
-	 :map org-mode-map
-	 ("C-M-i" . completion-at-point)
-	 )
-  )
+;; (use-package org-roam
+;;   :ensure t
+;;   :init
+;;   (setq org-roam-v2-ack t)
+;;   :custom
+;;   (org-roam-directory "~/orgblok/roam")
+;;   (org-roam-completion-everywhere t)
+;;   :bind (("C-c n l" . org-roam-buffer-toggle)
+;; 	 ("C-c n f" . org-roam-node-find)
+;; 	 ("C-c n i" . org-roam-node-insert)
+;; 	 :map org-mode-map
+;; 	 ("C-M-i" . completion-at-point)
+;; 	 )
+;;   )
 (setq org-startup-folded t)
 ;; (use-package all-the-icons)
 
